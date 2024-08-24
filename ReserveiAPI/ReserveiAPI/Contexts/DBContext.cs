4ec5a661ffc87607f261fb29f55f53eb8526c3a9
marcoantonio-dev <maarcoantoniomasson@gmail.com>
2024-08-24 10:07:15 -0300

    :sparkles: feat ModelUser, Context and ConnectionString.

---
+++ ReserveiAPI/ReserveiAPI/Contexts/DBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReserveiAPI.Contexts.Builders;
+using ReserveiAPI.Objects.Models.Entities;
+
+namespace ReserveiAPI.Contexts
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<DbContext> options) : base(options) { }
+
+        public DbSet<UserModel> User { get; set;}
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder) 
+        {
+            base.OnModelCreating(modelBuilder);
+
+            UserBuilder.Build(modelBuilder);
+        }
+
+
+    }
+}
