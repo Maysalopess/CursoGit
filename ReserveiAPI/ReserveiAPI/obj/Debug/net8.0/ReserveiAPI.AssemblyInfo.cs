5ef7cd8c589b6871ba445a2d1b726d6dd9ebe8be
marcoantonio-dev <maarcoantoniomasson@gmail.com>
2024-08-27 13:32:20 -0300

    :sparkles: feat UserControler

--- ReserveiAPI/ReserveiAPI/obj/Debug/net8.0/ReserveiAPI.AssemblyInfo.cs
+++ ReserveiAPI/ReserveiAPI/obj/Debug/net8.0/ReserveiAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReserveiAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56a5ba7813a49056c7cda2c26f732d2ffe18d185")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27565e5aa82e4fce30ab6913f382d73f48043c0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReserveiAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReserveiAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
